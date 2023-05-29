408f008e00bdc39ce44739aa4c04cded5b51aaeb
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-05-29 11:54:29 +0530

    moving from one activity to another done successfully

--- app/src/main/java/com/example/wordsapp/LetterAdapter.kt
+++ app/src/main/java/com/example/wordsapp/LetterAdapter.kt
@@ -15,6 +15,7 @@
  */
 package com.example.wordsapp
 
+import android.content.Intent
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
@@ -62,6 +63,11 @@ class LetterAdapter :
     override fun onBindViewHolder(holder: LetterViewHolder, position: Int) {
         val item = list.get(position)
         holder.button.text = item.toString()
+        holder.button.setOnClickListener {
+            val intent = Intent(holder.itemView.context,DetailActivity::class.java)
+            intent.putExtra("letter",holder.button.text.toString())
+            holder.itemView.context.startActivity(intent)
+        }
     }
 
     // Setup custom accessibility delegate to set the text read with
