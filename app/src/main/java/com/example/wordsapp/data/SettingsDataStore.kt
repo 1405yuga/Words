67981da83a5538b59ecf8ee7c428edc81a8c55b2
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-06 23:07:25 +0530

    preference datastore name

--- app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
+++ app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
@@ -2,5 +2,7 @@ package com.example.wordsapp.data
 
 import android.content.Context
 
+private const val LAYOUT_PREFERENCES_NAME = "layout_preferences"
 class SettingsDataStore(context : Context) {
+
 }
