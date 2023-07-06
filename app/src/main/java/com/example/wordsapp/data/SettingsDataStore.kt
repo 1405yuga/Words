e02780febe2f698889990cdb947a8873a4c0931b
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-06 23:17:26 +0530

    created key type

--- app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
+++ app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
@@ -1,8 +1,16 @@
 package com.example.wordsapp.data
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 
 private const val LAYOUT_PREFERENCES_NAME = "layout_preferences"
-class SettingsDataStore(context : Context) {
 
+//create instance of preference datastore
+private val Context.datastore : DataStore<Preferences> by preferencesDataStore(name = LAYOUT_PREFERENCES_NAME)
+
+class SettingsDataStore(context : Context) {
+    private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
 }
