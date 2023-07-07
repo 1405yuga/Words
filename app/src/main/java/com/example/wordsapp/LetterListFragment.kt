f7e666e63dda5ae0528afe10172221e5b20f1b1a
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-07 08:57:08 +0530

    updated and saved on linearlayoutchange

--- app/src/main/java/com/example/wordsapp/LetterListFragment.kt
+++ app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -8,11 +8,13 @@ import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wordsapp.data.SettingsDataStore
 import com.example.wordsapp.databinding.FragmentLetterListBinding
+import kotlinx.coroutines.launch
 
 
 class LetterListFragment : Fragment() {
@@ -64,6 +66,9 @@ class LetterListFragment : Fragment() {
                 return when(menuItem.itemId){
                     R.id.action_switch_layout->{
                         isLinearLayoutManager = !isLinearLayoutManager
+                        lifecycleScope.launch {
+                            settingsDataStore.saveLayoutPreferenceToDataStore(isLinearLayoutManager,requireContext())
+                        }
                         chooseLayout()
                         chooseIcon(menuItem)
                         true
