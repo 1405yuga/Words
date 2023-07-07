9b005ff64279b6c9576ee1fc153ea4b53b7b9bf0
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-07 07:56:26 +0530

    layout preference updated in datastore

--- app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
+++ app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
@@ -4,13 +4,23 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
 
 private const val LAYOUT_PREFERENCES_NAME = "layout_preferences"
 
 //create instance of preference datastore
-private val Context.datastore : DataStore<Preferences> by preferencesDataStore(name = LAYOUT_PREFERENCES_NAME)
+private val Context.datastore: DataStore<Preferences> by preferencesDataStore(name = LAYOUT_PREFERENCES_NAME)
 
-class SettingsDataStore(context : Context) {
+class SettingsDataStore(context: Context) {
     private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
+
+
+    suspend fun saveLayoutPreferenceToDataStore(isLinearLayoutManager: Boolean, context: Context) {
+
+        //edit() suspend function that transactionally updates the data in DataStore
+        context.datastore.edit { preferences ->
+            preferences[IS_LINEAR_LAYOUT_MANAGER] = isLinearLayoutManager
+        }
+    }
 }
